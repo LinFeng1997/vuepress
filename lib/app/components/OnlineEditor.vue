af80caa0002adbe5ad29f7c5985c7ca94140b580
许云峰 <xuyunfeng.fe@bytedance.com>
2018-08-23 20:11:27 +0800

    feat: OnlineEditor component

--- lib/app/components/OnlineEditor.vue
+++ lib/app/components/OnlineEditor.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    <textarea rows="10" :value="value" @input="write"/>
-    <component v-if="componentName" :is="componentName"></component>
+  <div class="vuepress-editor">
+    <div class="mde">
+      <textarea/>
+    </div>
+    <component class="preview" v-if="componentName" :is="componentName"></component>
   </div>
 </template>
 
 <script>
 import { writeMarkdown, initMarkdownFile } from '../../api/index'
 import Vue from 'vue'
+// import SimpleMDE from 'simplemde'
 export default {
   data () {
     return {
@@ -17,13 +20,21 @@ export default {
   },
   methods: {
     async write (e) {
+      console.log('aaa')
       const content = e.target.value
       await writeMarkdown(content)
     }
   },
   async mounted () {
+    const SimpleMDE = window.SimpleMDE || (() => {
+    })
+    // const simplemde = new SimpleMDE({ element: document.getElementById('mde') })
     this.value = await initMarkdownFile()
-    console.log(this.value)
+    const mde = new SimpleMDE()
+    mde.value(this.value)
+    mde.codemirror.on('change', async () => {
+      await writeMarkdown(mde.value())
+    })
 
     let OnlineMarkdown = await import('../.temp/online.md')
     OnlineMarkdown = OnlineMarkdown.default
@@ -34,8 +45,25 @@ export default {
   }
 }
 </script>
-<style>
-  textarea {
-    width: 100%;
+<style lang="stylus">
+  .vuepress-editor
+    display flex
+    .mde
+      width 50%
+      .CodeMirror,.CodeMirror-scroll
+        max-height 800px
+    .preview
+      width 50%
+      max-height 800px
+      overflow auto
+
+  /*覆盖 vuepress 样式*/
+  .CodeMirror pre {
+    background-color initial !important
+    line-height 1 !important
+    margin: 0 !important
+    padding: 0 4px !important
+    overflow: visible !important
   }
+
 </style>
---
+++ online/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+    navbar: false
+  },
+  head: [
+    ['script', {src: 'https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js'}],
+    ['link', {rel: 'stylesheet', type: 'text/css', href: 'https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css'}]
+  ],
+  sidebarDepth: 0
+}
