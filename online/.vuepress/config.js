006bc19958d0be9830b19840c39fe44847b9a800
许云峰 <xuyunfeng.fe@bytedance.com>
2018-08-23 21:10:56 +0800

    refactor: lib store in public file

--- online/.vuepress/config.js
+++ online/.vuepress/config.js
@@ -3,8 +3,8 @@ module.exports = {
     navbar: false
   },
   head: [
-    ['script', {src: 'https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js'}],
-    ['link', {rel: 'stylesheet', type: 'text/css', href: 'https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css'}]
+    ['script', {src: '/simplemde.min.js'}],
+    ['link', {rel: 'stylesheet', type: 'text/css', href: '/simplemde.min.css'}]
   ],
   sidebarDepth: 0
 }
